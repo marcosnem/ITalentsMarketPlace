56981bf7dd88d4f3e6de28faf41b0d65375d2f38
marcosnem <67987503+marcosnem@users.noreply.github.com>
2023-03-30 19:17:24 -0300

    Feat edição das rotas e construcao do usuario controller

--- src/router/usuario.router.js
+++ src/router/usuario.router.js
@@ -4,17 +4,17 @@ const usuarioController = require("../controller/usuario.controller")
 //funcao para trabalhar com rotas
 const router = express.Router();
 
-router.get('/findById:id');
-router.get('/findAll');
+router.get('/findById:id', usuarioController.findUserByIdController);
+router.get('/findAll', usuarioController.findAllUsersController);
 
-router.post('/create');
-router.post('/addAddress/:id');
-router.post('/addFavProduct/:id');
+router.post('/create', usuarioController.createUserController);
+router.post('/addAddress/:id', usuarioController.addUserAddressController);
+router.post('/addFavProduct/:id', usuarioController.addUserFavProduct);
 
-router.put('/update/:id');
+router.put('/update/:id', usuarioController.updateUserController);
 
-router.delete('/remove/:id');
-router.delete('/removoAddress/');
-router.delete('/removeFavProduct');
+router.delete('/remove/:id', usuarioController.removeUserController);
+router.delete('/removoAddress/', usuarioController.removeUserAddressController);
+router.delete('/removeFavProduct', usuarioController.removeUserFavProduct);
 
 module.exports = router;
