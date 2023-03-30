f70c1745be085d73d44754c4c5b33a900e6f950c
marcosnem <67987503+marcosnem@users.noreply.github.com>
2023-03-30 19:46:33 -0300

    Fix renomeando metodos dos controller

--- src/controller/usuario.controller.js
+++ src/controller/usuario.controller.js
@@ -116,7 +116,7 @@ const removeUserAddressController = async (req,res) => {
 
 };
 
-const addUserFavProduct = async (req,res) => {
+const addUserFavProductController = async (req,res) => {
     try{
 
    
@@ -133,7 +133,7 @@ const addUserFavProduct = async (req,res) => {
 
 };
 
-const removeUserFavProduct = async (req,res) => {
+const removeUserFavProductController = async (req,res) => {
     try{
 
    
@@ -159,6 +159,6 @@ module.exports = {
     removeUserController,
     addUserAddressController,
     removeUserAddressController,
-    addUserFavProduct,
-    removeUserFavProduct,
+    addUserFavProductController,
+    removeUserFavProductController,
 }
