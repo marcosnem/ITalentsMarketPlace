883322588866b3373b0fb5dcb2fd8282efd6daf1
marcosnem <marcos.zeh@gmail.com>
2023-04-07 16:49:37 -0300

    fix anotacoes produto service controller

---
+++ src/service/produto.service.js
@@ -0,0 +1,37 @@
+const Produto = require("../model/Produto");
+
+const findProductByIdService =  (id) => {
+   return Produto.findById(id);
+    
+}
+
+const findAllProductService = () => {
+
+    return Produto.find();
+
+}
+
+const createProductService = (body) => {
+return Produto.create(body);
+    
+}
+
+const updateProductService = (id,body) =>{
+    return Produto.findByIdAndUpdate(id,body, { returnDocument: "after" });
+
+}
+
+const deleteProductService = (id) => {
+    return Produto.findByIdAndRemove(id);
+
+}
+
+module.exports = {
+
+findProductByIdService,
+findAllProductService,
+createProductService,
+updateProductService,
+deleteProductService,
+
+}
