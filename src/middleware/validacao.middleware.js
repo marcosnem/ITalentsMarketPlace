8e2771cb99d611c83563d1a08191386ae5edc49c
marcosnem <67987503+marcosnem@users.noreply.github.com>
2023-04-13 19:45:13 -0300

    feat valida produto categoria

--- src/middleware/validacao.middleware.js
+++ src/middleware/validacao.middleware.js
@@ -20,8 +20,62 @@ const validaUsuario = (req, res, next) => {
   }
 
   return next();
-};
+}
+
+const validaProduto = (req, res, next) => {
+  let erros = [];  //var para acumular erros
+
+  //checar cada campo
+  if(!req.body.nome){
+    erros.push("nome");
+  }
+
+  if(!req.body.descricao){
+    erros.push("descricao");
+  }
+
+  if(!req.body.precoUnitario){
+    erros.push("precoUnitario");
+  }
+
+  if(!req.body.imagem){
+    erros.push("imagem");
+  }
+
+  if(!req.body.codigoBarra){
+    erros.push("codigoBarra");
+  }
+
+  //se nao tiver erros, passe para o next
+  if(erros.length == 0){
+    return next();
+
+} else {
+  //se for um erro
+    if(erros.length > 1){
+      return res.status(400).send({ message: `Os campos ${erros} precisam ser preenchidos!`  });
+    }else {
+      //se for mais de um
+      return res.status(400).send({ message: `O campo ${erros} precisa ser preenchido!`  });
+    }
+}
+    
+}
+
+const validaCategoria = (req, res, next) => {
+
+  if(!req.body.nome){
+
+    return res.status(400).send({ message: `O campo 'nome' precisa ser preenchido!`  });
+    
+  }
+
+  return next();
+
+}
 
 module.exports = {
-    validaUsuario
+    validaUsuario,
+    validaProduto,
+    validaCategoria
 }
